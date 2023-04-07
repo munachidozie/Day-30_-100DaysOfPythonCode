385468087dd774cba0ffd6b71013cc4bcbf42027
Munachimso Ukaoha <>
2023-04-07 22:35:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print("30 Days of Python Challenge Feedback")
+print()
+print("Tell us what your experience was on each day of te challenge")
+print()
+print()
+
+for i in range(1, 31):
+  print(input(f"""Day {i}: \n"
+  On this day, """))
+  print()
+  print()
